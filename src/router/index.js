ec06d874a0560047ad069f5d1de5067e483f039d
Juan Kaenel <kaenel10@gmail.com>
2020-12-08 16:24:13 -0300

    Componente login

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Category from '../components/Category'
+import Login from '../components/Login'
 Vue.use(VueRouter)
 
 const routes = [
@@ -11,18 +12,16 @@ const routes = [
     component: Home
   },
   {
+    path: '/login',
+    name: 'Login', 
+    component: Login
+  },
+  {
     path: '/category',
     name: 'Category', // nombre que usamos en el :to="{name: 'category'}"
     component: Category
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+
 ]
 
 const router = new VueRouter({
