fb9d9ec0ea107bee19a110100227a78385833d7b
Juan Kaenel <kaenel10@gmail.com>
2021-01-27 09:09:26 -0300

    :art: Implementando chart.js para los gráficos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,25 @@
 <template>
-  <h1>Home</h1>
+  
 </template>
 
 <script>
+import axios from 'axios';
+import Chart from 'chart.js';
 
 export default {
-  name: 'Home',
-  components: {
+data(){
+  return{
+    values:[], // almaceno todos los meses con sus totales
+    months:[], // almaceno los meses
+    totals:[], // almaceno los totales
   }
+},
+methods:{
+
+},
+mounted(){
+
+} 
 }
+
 </script>
